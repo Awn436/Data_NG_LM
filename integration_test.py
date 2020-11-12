b9d85c3675c97a62a7afcabe43c409364ed08130
Men0x <38214356+Men0x@users.noreply.github.com>
2020-11-12 10:04:14 +0100

    integration_test fixed

--- integration_test.py
+++ integration_test.py
@@ -7,18 +7,19 @@ from sklearn.feature_extraction.text import CountVectorizer
 from training_model import model_training
 from data_preprocess import data_preprocessing
 
-df = pd.read_csv('training.1600000.processed.noemoticon.csv', encoding="ISO-8859-1", names=["target", "ids", "date", "flag", "user", "text"])
-data_preprocessing(df)
-model_training(df)
-a = model.predict(vec.transform(pd.Series('I am good')))
-b = model.predict(vec.transform(pd.Series('I am bad')))
-
 def good_bad(x):
     if x==4:
-        print("this is good")
+        print("Positive")
     if x==0:
-        print("this is bad")
+        print("Negative")
     return 0
 
-good_bad(a)
-good_bad(b)
+if __name__ == "__main__":
+
+    df = pd.read_csv('training.1600000.processed.noemoticon.csv', encoding="ISO-8859-1", names=["target", "ids", "date", "flag", "user", "text"])
+    df = data_preprocessing(df)
+    model, acc, vec = model_training(df)
+    a = model.predict(vec.transform(pd.Series('I am good')))
+    b = model.predict(vec.transform(pd.Series('I am bad')))
+    good_bad(a)
+    good_bad(b)
